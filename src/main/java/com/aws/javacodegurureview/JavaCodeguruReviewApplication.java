56a3e7056a4755654251788148a3673d4b11761e
Phati <phatisavant@gmail.com>
2023-02-03 14:44:47 +0530

    addded project files

---
+++ src/main/java/com/aws/javacodegurureview/JavaCodeguruReviewApplication.java
@@ -0,0 +1,13 @@
+package com.aws.javacodegurureview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeguruReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeguruReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/javacodegurureview/JavaCodeguruReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.javacodegurureview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeguruReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
